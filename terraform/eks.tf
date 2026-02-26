93862b346946b9a42a1b5612c34c070b8efbc569
wisdom <wisdomolaniyan@yahoo.com>
2026-02-26 22:41:44 +0000

    update

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 20.31"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.34"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
