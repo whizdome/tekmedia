d1467dcf50ad2dd55e5f1d460cf5a012888c2868
wisdom <wisdomolaniyan@yahoo.com>
2026-02-26 18:54:23 +0000

    kubectl update

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -8,6 +8,9 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
   eks_managed_node_groups = {
     default = {
       instance_types = ["t2.micro"]
